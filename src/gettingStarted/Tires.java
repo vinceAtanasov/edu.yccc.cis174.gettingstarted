6130b04b73731f3e110c6b02aaada3391c4728be
velik <velik@Vince-Dell>
2018-01-24 19:55:32 -0500

    update

--- src/gettingStarted/Tires.java
+++ src/gettingStarted/Tires.java
@@ -5,9 +5,32 @@ public class Tires {
 	private String type;
 	private String size;
 	private boolean runFlat;
-
 	
-	public void testMe() {
-		System.out.println("Xaxa");
+	public Tires() {
+		
 	}
-}
+	
+	public Tires(String brand, String type, String size, boolean runFlat) {
+		this.brand = brand;
+		this.type = type;
+		this.size = size;
+		this.runFlat = runFlat;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public void setSize(String size) {
+		this.size = size;
+	}
+	
+	public void setRunFlat(boolean runFlat) {
+		this.runFlat = runFlat;
+	}
+	
+	}
