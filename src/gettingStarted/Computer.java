8397f523106a931ee98320d3e0a0513ceaa5e935
velik <velik@Vince-Dell>
2018-01-24 19:05:38 -0500

    edit

--- src/gettingStarted/Computer.java
+++ src/gettingStarted/Computer.java
@@ -1,5 +1,10 @@
 package gettingStarted;
 
 public class Computer {
-
+	private String motherBoard;
+	private String cpu;
+	private String ram;
+	private String hardDrive;
+	private String os;
+	
 }
