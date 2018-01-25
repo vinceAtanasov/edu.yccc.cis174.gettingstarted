cbf5a96b2c2c3ec5e21185bfbb31916d3e026517
velik <velik@Vince-Dell>
2018-01-25 09:23:49 -0500

    update

--- src/gettingStarted/Tire.java
+++ src/gettingStarted/Tire.java
@@ -1,52 +1,52 @@
 package gettingStarted;
 
-public class Tires {
+public class Tire {
 	private String brand;
 	private String type;
 	private String size;
 	private boolean runFlat;
-	
-	public Tires() {
-		
+
+	public Tire() {
+
 	}
-	
-	public Tires(String brand, String type, String size, boolean runFlat) {
+
+	public Tire(String brand, String type, String size, boolean runFlat) {
 		this.brand = brand;
 		this.type = type;
 		this.size = size;
 		this.runFlat = runFlat;
 	}
-	
+
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
-	
+
 	public void setType(String type) {
 		this.type = type;
 	}
-	
+
 	public void setSize(String size) {
 		this.size = size;
 	}
-	
+
 	public void setRunFlat(boolean runFlat) {
 		this.runFlat = runFlat;
 	}
-	
+
 	public String getBrand() {
 		return brand;
 	}
-	
-	 public String getType() {
-		 return type;
-	 }
-	 
-	 public String getSize() {
-		 return size;
-	 }
-	 
-	 public boolean getRunFlat() {
-		 return runFlat;
-	 }
-	
+
+	public String getType() {
+		return type;
 	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public boolean getRunFlat() {
+		return runFlat;
+	}
+
+}
